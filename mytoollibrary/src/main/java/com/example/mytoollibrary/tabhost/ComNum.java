df5d4596eb6dcd96d818c06bbc8bdf74931f02cb
898724271@qq.com <wap15908486802>
2020-04-02 18:15:39 +0800

    底部导航

---
+++ mytoollibrary/src/main/java/com/example/mytoollibrary/tabhost/ComNum.java
@@ -0,0 +1,13 @@
+package com.example.mytoollibrary.tabhost;
+
+/**
+ * @author qiao
+ * @date 2018/1/4 下午 5:27
+ */
+
+public class ComNum {
+    /**
+     * 底部tabhost菜单中每个标签宽度是屏幕的 TABHOST_WIDTH/1
+     */
+    public static final int TABHOST_WIDTH = 5;
+}
